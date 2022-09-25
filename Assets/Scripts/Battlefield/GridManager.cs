9075540e5d4206d7be7da7216da4993952b6d721
Matt <matthewwilliam.cole@gmail.com>
2022-09-25 22:41:30 +0100

    added prefab

---
+++ Assets/Scripts/Battlefield/GridManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattlefieldManager : MonoBehaviour
+{
+    public int ranks;
+    public int files;
+
+    void Start()
+    {
+        
+    }
+    void BuildBattlefield()
+    {
+        for (int x = 0; x < files; x++)
+        {
+            for (int y = 0; y < ranks; y++)
+            {
+                
+            }
+        }
+    }
+}
